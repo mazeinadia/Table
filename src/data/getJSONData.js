f991e0c65a6873160a90956d0f5878f6ad94a69e
Nadia M <leavingofnight@gmail.com>
2018-07-26 17:48:59 +0500

    json

---
+++ src/data/getJSONData.js
@@ -0,0 +1,5 @@
+import data from './data.json';
+
+export default function () {
+    return data;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/app.jsx';
+import App from './app/app.jsx';
+import getData from './data/getJSONData'
 
 const app = document.getElementById('app');
+getData();
 
 ReactDOM.render(<App />, app);
